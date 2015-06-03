69f62ee4456ecf40a94a8a552d9202c6f90e1c2b
Ed Summers <ehs@pobox.com>
2015-06-03 14:06:37 -0400

    moved into the same directory

--- lib/jekyll-wikidata.rb
+++ lib/jekyll-wikidata.rb
@@ -3,5 +3,5 @@ require 'jekyll'
 require 'wikidata'
 
 require 'jekyll-wikidata/version'
-require 'jekyll/commands/wikidata'
-require 'jekyll/wikidata/writer'
+require 'jekyll-wikidata/add'
+require 'jekyll-wikidata/writer'
